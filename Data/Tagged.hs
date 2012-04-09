856fc07cf966069ceeef0c4ef6c0969edfe03639
Edward Kmett <ekmett@gmail.com>
2012-04-09 10:53:06 -0400

    removed no-longer-used Text.Read dependency

--- Data/Tagged.hs
+++ Data/Tagged.hs
@@ -30,7 +30,6 @@ import Data.Foldable (Foldable(..))
 import Data.Data (Data,Typeable)
 #endif
 import Data.Ix (Ix(..))
-import Text.Read
 
 -- | A @'Tagged' s b@ value is a value @b@ with an attached phantom type @s@.
 -- This can be used in place of the more traditional but less safe idiom of
